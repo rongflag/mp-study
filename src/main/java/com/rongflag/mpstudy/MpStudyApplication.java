a702e554ae9de6d27d4d6c3bac0529711708eca7
rongflag <worongbo@163.com>
2019-12-10 13:44:45 +0800

    起一个项目

---
+++ src/main/java/com/rongflag/mpstudy/MpStudyApplication.java
@@ -0,0 +1,13 @@
+package com.rongflag.mpstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MpStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rongflag/mpstudy/MpStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rongflag.mpstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MpStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
